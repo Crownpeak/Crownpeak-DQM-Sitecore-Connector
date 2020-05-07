aed4f7bedb525f74bc36f84da659fbdef8c971bd
Prantik Das <prantik.das@crownpeak.com>
2020-05-07 16:41:14 +0530

    OCD-18598/sitecore-82

---
+++ src/Cognifide.Sitecore.Crownpeak/DAO/DAOErrorLog.cs
@@ -0,0 +1,42 @@
+﻿using Cognifide.Sitecore.Crownpeak.Logic;
+using System;
+using System.IO;
+using System.Web.Hosting;
+
+namespace Cognifide.Sitecore.Crownpeak.DAO
+{
+    public class DAOErrorLog
+    {
+        private static DAOErrorLog instance;
+
+        private DAOErrorLog() { }
+
+        public static DAOErrorLog GetInstance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new DAOErrorLog();
+                }
+                return instance;
+            }
+        }
+
+        internal void ErrorLogWrite(string className, string methodName, string errorMsg)
+        {
+            if (Settings.Log == "1")
+            {
+                string errorMessage = string.Format("Log at : {0}| Method Name : {1} | {2} --> {3}", DateTime.Now.ToString(), className, methodName, errorMsg);
+
+                string filePath = HostingEnvironment.ApplicationPhysicalPath + @"/Error_Log.txt";
+                StreamWriter sw = new StreamWriter(filePath, true);
+
+                sw.WriteLine(errorMessage);
+                sw.Flush();
+                sw.Close();
+            }
+        }
+    }
+
+}
